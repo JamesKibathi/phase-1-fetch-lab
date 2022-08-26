41b107ab1e56b96fac27ee18d91f288d83a21a94
James Njenga <knjenga94@gmail.com>
2022-08-27 00:53:30 +0300

    Completed assignment

--- index.js
+++ index.js
@@ -1,5 +1,12 @@
 function fetchBooks() {
   // To pass the tests, don't forget to return your fetch!
+
+ return fetch("https://anapioficeandfire.com/api/books")
+  .then((resp) => resp.json())
+  .then((data)=>{renderBooks(data)})
+
+ //.then((data)=>console.log(data))
+ 
   
 }
 
